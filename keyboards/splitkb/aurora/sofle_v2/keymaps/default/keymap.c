f696e38a329b5dc711c37e12965edde95dba2aa5
JRehkemper <jannik.rehkemper@gmail.com>
2024-02-24 13:57:33 +0100

    fix timer for animation

--- keyboards/splitkb/aurora/sofle_v2/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/default/keymap.c
@@ -32,19 +32,19 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-            KC_ESC,     DE_1,       DE_2,       DE_3,       DE_4,       DE_5,                               DE_6,       DE_7,       DE_8,       DE_9,       DE_0,       DE_SS,
-            KC_TAB,     DE_Q,       DE_W,       DE_E,       DE_R,       DE_T,                               DE_Z,       DE_U,       DE_I,       DE_O,       DE_P,       DE_UDIA,
-            KC_LSFT,    DE_A,       DE_S,       DE_D,       DE_F,       DE_G,                               DE_H,       DE_J,       DE_K,       DE_L,       DE_ODIA,    DE_ADIA,
-            DE_LABK,    DE_Y,       DE_X,       DE_C,       DE_V,       DE_B,   _______,        _______,    DE_N,       DE_M,       DE_COMM,    DE_DOT,     DE_MINS,    DE_HASH,
-                                    KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,KC_BSPC,        KC_ENT,     KC_SPC,     MO(1),      KC_RALT,    KC_RGUI
+            KC_ESC,     DE_1,       DE_2,           DE_3,           DE_4,           DE_5,                                   DE_6,       DE_7,           DE_8,           DE_9,           DE_0,       DE_SS,
+            KC_TAB,     DE_Q,       DE_W,           DE_E,           DE_R,           DE_T,                                   DE_Z,       DE_U,           DE_I,           DE_O,           DE_P,       DE_UDIA,
+            KC_LSFT,    DE_A,       DE_S,           DE_D,           DE_F,           DE_G,                                   DE_H,       DE_J,           DE_K,           DE_L,           DE_ODIA,    DE_ADIA,
+            DE_LABK,    DE_Y,       LALT_T(DE_X),   LGUI_T(DE_C),   LCTL_T(DE_V),   DE_B,       _______,        _______,    DE_N,       LCTL_T(DE_M),   LGUI_T(DE_COMM),LALT_T(DE_DOT), DE_MINS,    DE_HASH,
+                                    KC_RALT,        KC_LCTL,        MO(1),          KC_LSFT,    KC_BSPC,        KC_ENT,     KC_SPC,     MO(1),          KC_LCTL,        KC_RALT
     ),
 
 	[1] = LAYOUT(
             DE_CIRC,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
             _______,    DE_LBRC,    DE_RBRC,    DE_LCBR,    DE_RCBR,    _______,                            DE_LPRN,    DE_RPRN,    DE_PLUS,    DE_MINS,    _______,    KC_F12,
             _______,    DE_AT,      DE_AT,      DE_HASH,    DE_DLR,     DE_PERC,                            KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
-            DE_PIPE,    DE_EQL,     DE_MINS,    DE_PLUS,    DE_LCBR,    DE_RCBR, _______,       _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,
-                                    _______,    _______,    _______,    _______, _______,       _______,    _______,    _______,    _______,    _______
+            DE_PIPE,    DE_EQL,     DE_MINS,    DE_PLUS,    DE_LCBR,    DE_RCBR, _______,       _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    DE_ACUT,
+                                    _______,    _______,    _______,    _______, KC_DEL,        _______,    _______,    _______,    _______,    _______
     )
 };
 
