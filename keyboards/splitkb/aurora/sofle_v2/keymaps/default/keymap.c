a9e48b32ccc2f843e0e11f3f167193280c2d1da0
JRehkemper <jannik.rehkemper@gmail.com>
2025-02-24 14:29:46 +0100

    add mouse keys

--- keyboards/splitkb/aurora/sofle_v2/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/default/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT(
             DE_CIRC,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
             _______,    DE_LBRC,    DE_RBRC,    DE_LCBR,    DE_RCBR,    _______,                            DE_LPRN,    DE_RPRN,    DE_PLUS,    DE_MINS,    _______,    KC_F12,
-            _______,    DE_AT,      DE_AT,      DE_HASH,    DE_DLR,     DE_PERC,                            KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
-            DE_PIPE,    DE_EQL,     DE_MINS,    DE_PLUS,    DE_LCBR,    DE_RCBR, _______,       _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    DE_ACUT,
+            _______,    MS_LEFT,    MS_DOWN,    MS_UP,      MS_RIGHT,   DE_PERC,                            KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
+            DE_PIPE,    DE_EQL,     MS_BTN1,    MS_BTN2,    MS_BTN3,    DE_RCBR, _______,       _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    DE_ACUT,
                                     _______,    _______,    _______,    _______, KC_DEL,        _______,    _______,    _______,    _______,    _______
     )
 };
--- keyboards/splitkb/aurora/sofle_v2/rules.mk
+++ keyboards/splitkb/aurora/sofle_v2/rules.mk
@@ -1,2 +1,3 @@
 DEFAULT_FOLDER = splitkb/aurora/sofle_v2/rev1
 WPM_ENABLE = yes
+MOUSEKEY_ENABLE = yes
